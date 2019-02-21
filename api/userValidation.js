aa65a551bdb5638e8d8b741669392d043d816aac
Tommaso Piazza <tommaso.piazza@gmail.com>
2019-02-21 12:03:30 +0100

    Make it work

--- api/userValidation.js
+++ api/userValidation.js
@@ -2,9 +2,12 @@ const crypto = require('crypto');
 
 const usersStorage = new Map();
 
+
+// ((?:DEV|PRD)__[0-9a-f]{14}_\d{8}|drozimport_attrib__[a-z09-]*|(?:OIAM_|(?:SSO|AUTHMGT)-\d+)?[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}|[0-9a-f]{14}_[0-9a-f]{8})_([\w-]*\.[\w-]*\.[\w-]{86})
+
 const generateUserToken = () => crypto.randomBytes(32).toString('base64');
 const pseudoEncodeToken = (identity, token) => usersStorage.set(token, identity);
-const pseudoDecodeToken = (token) => usersStorage.get(token);
+const pseudoDecodeToken = (token) => token.split('_')[0];
 const pseudoVerifyToken = (token) => true;
 
 const requireAuthHeader = (req, res, next) => {
